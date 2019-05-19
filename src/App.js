a29eb95f86a0dddf7c9bdcf956d85379a9ae4a44
Todor Imreorov <blurymind@gmail.com>
2019-05-19 14:15:13 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Viewer from './Viewer/'; 
@@ -9,20 +8,6 @@ class App extends Component {
     return (
       <div className="App">
       <Viewer />
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
       </div>
     );
   }
